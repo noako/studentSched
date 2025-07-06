b81963a11513196967a5e0ef04f4124df366bc79
noaja <noakoffman@gmail.com>
2025-07-06 20:30:19 +0300

    Adding prisma and DB connection with new User model

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const authRoutes = require('./routes/auth');
+const prisma = require('./db/client');
 
 // Middleware
 app.use(express.json());
@@ -10,9 +11,17 @@ app.use(express.json());
 app.use(cors({origin: true, credentials: true})); // Enable CORS for all origins
 app.use('/api/auth', authRoutes);
 // Routes
+
+app.get('/api/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
+
+
 
 // CRON Jobs
 require('./cron');
